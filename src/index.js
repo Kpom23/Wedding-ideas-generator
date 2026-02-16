e96acb0326f74f6d70fa910bf6bbbc1a4de5c14f
Kpom23 <143014073+Kpom23@users.noreply.github.com>
2026-02-15 19:18:58 -0600

    added the basic form handing and added typewriting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("hello world");
+function generateIdea(event) {
+    event.preventDefault()
+
+    new Typewriter('#idea', {
+  strings: ["Set a budget"],
+  autoStart: true,
+  delay: 1,
+  cursor:"",
+
+    });
+
+}
+
+
+let ideaFormElement = document.querySelector("#idea-generator-form");
+ideaFormElement.addEventListener("submit", generateIdea)
